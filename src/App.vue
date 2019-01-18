55bd8d38c7036efe5e5ff0343a50b9277ee59f41
Danilo Tiago <programacao.desenvolvimento@gmail.com>
2019-01-18 19:40:37 -0200

    configurando rotas dinamicamente e nomeando os titulos das rotas

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <template>
   <div class="corpo">
-    <router-view></router-view>
+    <nav>
+      <ul>
+        <li v-for="route in routes">
+          <router-link :to="route.path">{{route.titulo}}</router-link>
+        </li>
+      </ul>
+    </nav>
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
+  import { routes } from "./routes"
 
-export default {
-}
+  export default {
+    data() {
+      return {
+        routes
+      }
+    }
+  }
 </script>
 
 <style>
@@ -15,5 +30,11 @@ export default {
     font-family: Helvetica, sans-serif;
     margin: 0 auto;
     width: 96%;
+  }
+  .pagina-enter, .pagina-leave-active {
+    opacity: 0
+  }
+  .pagina-enter-active, .pagina-leave-active {
+    transition: opacity .4s
   }
 </style>
