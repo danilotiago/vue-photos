cb5d2cc423484357a7eb42cc1a7d414f594a09a9
Danilo Tiago <programacao.desenvolvimento@gmail.com>
2019-01-18 19:21:53 -0200

    criando rotas e configurando a aplicacao

--- src/main.js
+++ src/main.js
@@ -2,11 +2,23 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource'
 import VueRouter from 'vue-router'
+// foi usado { } pois devemos explicitar o que importar do modulo pois routes nao esta
+// como exports default
+import { routes } from './routes'
 
 Vue.use(VueResource)
 Vue.use(VueRouter)
 
+// configuramos o roteador que recebe um objeto de rotas
+const router = new VueRouter({ routes })
+
+// caso o servidor esteja preparado para trabalhar sem rota hash "/#/" habilitamos a constante
+// abaixo com mode history
+// const router = new VueRouter({ routes, mode: 'history' })
+
+
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
