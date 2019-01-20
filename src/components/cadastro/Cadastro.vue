3b30ae44c2cff15f9b996c5e513a4cd5f00e524c
Danilo Tiago <programacao.desenvolvimento@gmail.com>
2019-01-20 14:53:49 -0200

    enviando dados para API

--- src/components/cadastro/Cadastro.vue
+++ src/components/cadastro/Cadastro.vue
@@ -61,8 +61,12 @@
     },
     methods: {
       grava() {
-        console.log(this.foto)
-        this.foto = new Foto()
+        this.$http
+          .post('http://localhost:3000/v1/fotos', this.foto)
+          .then(
+            () => this.foto = new Foto(),
+            err => console.log(err)
+          )
       }
     }
   }
